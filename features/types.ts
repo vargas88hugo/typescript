004b9bd3d762d967268e256ec8766c72d9e404f7
Hugo Vargas <vargas88hugo@gmail.com>
2020-04-06 17:17:06 -0500

    [Feature] Practice about type inference

---
+++ features/types.ts
@@ -0,0 +1,16 @@
+/**
+ * Typescript check type inference automatically
+ * and tries to figure out what type it is
+ */
+
+const today = new Date();
+
+today.getMonth();
+
+const person = {
+  age: 20,
+};
+
+class Color {}
+
+const red = new Color();
