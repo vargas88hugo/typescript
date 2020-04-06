592fcdfc1f1968d60d71cddfc56e548e5506a18f
Hugo Vargas <vargas88hugo@gmail.com>
2020-04-06 18:44:04 -0500

    [Feature] Practice about objects and destructiring with annotations

---
+++ features/annotations/objects.ts
@@ -0,0 +1,16 @@
+const profile = {
+  name: 'Hugo',
+  age: 31,
+  coords: {
+    lat: 0,
+    lng: 15,
+  },
+  setAge(age: number): void {
+    this.age = age;
+  },
+};
+// destructuring
+const { age }: { age: number } = profile;
+const {
+  coords: { lat, lng },
+}: { coords: { lat: number; lng: number } } = profile;
