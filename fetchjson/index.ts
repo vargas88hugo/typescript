4dc1256103ac53360a646c1b54e71cbb5e63ebc2
Hugo Vargas <vargas88hugo@gmail.com>
2020-04-06 17:05:50 -0500

    [Update] Implemented a function for catching erros in parameters

--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 /**
  * This lesson I learn how to implement an
- * interface to catch erros
+ * interface to catch errors
  */
 
 const url: string = 'https://jsonplaceholder.typicode.com/todos/1';
@@ -20,9 +20,13 @@ axios.get(url).then((response) => {
   const title = todo.title;
   const completed = todo.completed;
 
+  logTodo(id, title, completed)
+});
+
+const logTodo = (id: number, title: string, completed: boolean) {
   console.log(`
     The todo with id: ${id}
     Has a title of: ${title}
     Is it completed? ${completed}
   `);
-});
+}
