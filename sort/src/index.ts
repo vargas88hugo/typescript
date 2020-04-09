e26561129e30268dae2fdea21592debde87a2236
Hugo Vargas <vargas88hugo@gmail.com>
2020-04-08 20:39:56 -0500

    [Update] Improved program with the implementarion of an abstract class for using the method sor directly

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,16 +1,13 @@
-import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
 import { LinkedList } from './LinkedList';
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
-const sorterNumbers = new Sorter(numbersCollection);
-sorterNumbers.sort();
+numbersCollection.sort();
 console.log(numbersCollection.data);
 
 const charactersCollection = new CharactersCollection('asdasdas');
-const sorterCharacters = new Sorter(charactersCollection);
-sorterCharacters.sort();
+charactersCollection.sort();
 console.log(charactersCollection.data);
 
 const linkedList = new LinkedList();
@@ -18,6 +15,5 @@ linkedList.add(10);
 linkedList.add(3);
 linkedList.add(-5);
 linkedList.add(0);
-const sorterLinkedList = new Sorter(linkedList);
-sorterLinkedList.sort();
+linkedList.sort();
 linkedList.print();
