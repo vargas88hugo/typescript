2a383da2c1bbec3bd54f31bdcaa97709c8ac4564
Hugo Vargas <vargas88hugo@gmail.com>
2020-06-25 18:33:22 -0500

    [Feature] Implement MySQL configuration and connected with express

--- express-mysql/index.ts
+++ express-mysql/index.ts
@@ -1,9 +1,12 @@
 import Server from './src/server/server';
 import router from './src/router/router';
+import MySql from './src/mysql/mysql';
 
 const server = Server.init(3000);
 
 server.app.use(router);
+
+const mysql = new MySql();
 
 server.start(() => {
   console.log('Server running on port 3000');
---
+++ express-mysql/src/mysql/mysql.ts
@@ -0,0 +1,34 @@
+import mysql from 'mysql';
+
+export default class MySql {
+  private static _instance: MySql;
+
+  connection: mysql.Connection;
+  connected = false;
+
+  constructor() {
+    console.log('Initialized MySql Class');
+
+    this.connection = mysql.createConnection({
+      host: 'localhost',
+      user: 'node',
+      password: process.env.PASSWORD_MYSQL,
+      database: 'node',
+    });
+
+    this.connectDB();
+  }
+
+  private connectDB() {
+    this.connection.connect((err: mysql.MysqlError) => {
+      if (err) {
+        console.log(err.message);
+        return;
+      }
+
+      this.connected = true;
+
+      console.log('Database Online!');
+    });
+  }
+}
